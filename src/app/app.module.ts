ef14227bb7d6224e60466430e819572f57ea6c10
Esteban Droguett <estebandroguetthuerta@gmail.com>
2019-11-28 00:44:22 -0300

    Template del sistema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { APP_ROUTES } from './app.routes';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shader/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shader/header/header.component';
+import { SidebarComponent } from './shader/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shader/breadcrumbs/breadcrumbs.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesComponent } from './pages/pages.component';
+import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    ProgressComponent,
+    PagesComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTES
   ],
   providers: [],
   bootstrap: [AppComponent]
